2e5e588fc23deaa54a3d50c95d232ee85bb1ec4c
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-29 14:23:34 -0300

    ngIf

---
+++ diretivas-proj/src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit{
+  produtos:string[] = []
+
+  constructor() {
+    this.produtos = [
+      "mouse",
+      "teclado",
+      "cabo",
+      "fonte"
+    ]
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}

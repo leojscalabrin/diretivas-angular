da4e77e7ca307b3d8fdfbadf0c118ed3d2438a7b
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-10-02 14:28:18 -0300

    ngStyle

--- diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
+++ diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class CompAtributosComponent {
   estilo:string="enable"
+  corFundo:string = "blue"
+  corDaFonte:string = "white"
+
   constructor() {
 
   }
